de7237e55bf3a1696dfa14a61a9552173b9f3db7
“Henrique <henrique013.a@gmail.com>
2016-09-07 22:33:32 -0300

    cadastro/paciente

---
+++ www/public/bower/bottlejs/src/api.js
@@ -0,0 +1,49 @@
+
+/**
+ * Bottle constructor
+ *
+ * @param String name Optional name for functional construction
+ */
+var Bottle = function Bottle(name) {
+    if (!(this instanceof Bottle)) {
+        return Bottle.pop(name);
+    }
+
+    this.id = id++;
+    this.container = {
+        $register : register.bind(this),
+        $list : list.bind(this)
+    };
+};
+
+/**
+ * Bottle prototype
+ */
+Bottle.prototype = {
+    constant : constant,
+    decorator : decorator,
+    defer : defer,
+    digest : digest,
+    factory : factory,
+    instanceFactory: instanceFactory,
+    list : list,
+    middleware : middleware,
+    provider : provider,
+    register : register,
+    resolve : resolve,
+    service : service,
+    value : value
+};
+
+/**
+ * Bottle static
+ */
+Bottle.pop = pop;
+Bottle.list = list;
+
+/**
+ * Global config
+ */
+var globalConfig = Bottle.config = {
+    strict : false
+};
